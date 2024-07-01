d6f7fb13d07a7e0cc9c06e12d6d3f7cefa7ddd38
unknown <allanfulcher@gmail.com>
2024-07-01 16:20:45 -0300

    primeiro commit

---
+++ data.js
@@ -0,0 +1,563 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-oficina-do-churrasco",
+      "name": "oficina do churrasco",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.236415411034935,
+          "pitch": -0.0011804118833680377,
+          "rotation": 0,
+          "target": "7-texaco-frente"
+        },
+        {
+          "yaw": -0.8232226299651586,
+          "pitch": 0.030587000975746292,
+          "rotation": 0,
+          "target": "9-vagas"
+        },
+        {
+          "yaw": 1.5456320548368812,
+          "pitch": 0.21248807033229866,
+          "rotation": 0,
+          "target": "2-lago"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.3489216706487106,
+          "pitch": 0.02708845061403764,
+          "title": "Oficina do Churrasco",
+          "text": "Uma área temática, com peças de carro,uma pia e fogão para uso comum. Um bom local para fazer uma fogueira na parte externa."
+        },
+        {
+          "yaw": -2.940765363292785,
+          "pitch": -0.05399466533050301,
+          "title": "Bushome Gramado",
+          "text": "Nossa Hospedagem mais famosa, um onibus com uma decoração toda retro espera por você!"
+        }
+      ]
+    },
+    {
+      "id": "1-texaco",
+      "name": "texaco",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7129290679135174,
+          "pitch": -0.10431301182864239,
+          "rotation": 0,
+          "target": "7-texaco-frente"
+        },
+        {
+          "yaw": -0.5739031016161569,
+          "pitch": 0.17092815904558378,
+          "rotation": 0,
+          "target": "2-lago"
+        },
+        {
+          "yaw": -1.3926830629106934,
+          "pitch": -0.02140687662593521,
+          "rotation": 0,
+          "target": "3-estrada"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.416260754123419,
+          "pitch": -0.20189516100195526,
+          "title": "Kombi dos Sonhos",
+          "text": "hospedagem exclusiva do Gramado Motorpark a kombi dos sonhos tem um estilo rustico com uma temática hippie"
+        },
+        {
+          "yaw": 2.827039956077048,
+          "pitch": -0.2869580259208764,
+          "title": "Casa texaco frente",
+          "text": "A vista de dentro da casa texaco é espetacular."
+        }
+      ]
+    },
+    {
+      "id": "2-lago",
+      "name": "lago",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.30651860991748237,
+          "pitch": 0.016930287862498616,
+          "rotation": 0,
+          "target": "4-piscina"
+        },
+        {
+          "yaw": -0.9946182660769143,
+          "pitch": -0.023774229972797656,
+          "rotation": 0,
+          "target": "3-estrada"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.45478999193825054,
+          "pitch": 0.027293423688448115,
+          "title": "Balanço no Lago",
+          "text": "Um bom local para uma pausa, relaxar e aproveitar a natureza é uma das melhores coisas a fazer aqui no Gramado Motorpark"
+        },
+        {
+          "yaw": 1.1177561304936194,
+          "pitch": 0.2310119517204825,
+          "title": "Lago",
+          "text": "O lago conta com peixes ornamentais e patos que são uma verdaderia terapia."
+        }
+      ]
+    },
+    {
+      "id": "3-estrada",
+      "name": "estrada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3980434010289287,
+          "pitch": -0.13332936843462662,
+          "rotation": 0,
+          "target": "0-oficina-do-churrasco"
+        },
+        {
+          "yaw": 0.8345940963999396,
+          "pitch": 0.20820612800820726,
+          "rotation": 0,
+          "target": "2-lago"
+        },
+        {
+          "yaw": 1.9217830510114515,
+          "pitch": 0.017596412735422007,
+          "rotation": 0,
+          "target": "1-texaco"
+        },
+        {
+          "yaw": -1.7223171445976426,
+          "pitch": -0.009304113192634134,
+          "rotation": 0,
+          "target": "6-visual-patio"
+        },
+        {
+          "yaw": -0.22506762629469712,
+          "pitch": 0.17435572095251928,
+          "rotation": 0,
+          "target": "4-piscina"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.956773532215804,
+          "pitch": -0.3085489569423423,
+          "title": "Do alto do Trailer",
+          "text": "O trailer tem uma vista linda do patio, pela perspectiva do quarto de casal."
+        }
+      ]
+    },
+    {
+      "id": "4-piscina",
+      "name": "piscina",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4934289628807091,
+          "pitch": 0.1746947980556044,
+          "rotation": 0,
+          "target": "2-lago"
+        },
+        {
+          "yaw": 3.0507711263097583,
+          "pitch": -0.0410283668910445,
+          "rotation": 0,
+          "target": "3-estrada"
+        },
+        {
+          "yaw": -1.8667128923781142,
+          "pitch": -0.010432107757479159,
+          "rotation": 0,
+          "target": "5-entrada"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4296360288008465,
+          "pitch": 0.1022190829216747,
+          "title": "Área para festas",
+          "text": "A piscina é uma área muito alugada para festas e eventos. com churrasqueira e cozinha. Não podemos esquecer de nossas festas de fim de ano que ocorrem aqui e são um sucesso."
+        },
+        {
+          "yaw": -2.6413760795767054,
+          "pitch": -0.044505456094452,
+          "title": "Cascatinha",
+          "text": "Um local belo e tranquilo para ouvir o barulho da agua e apreciar a natureza."
+        }
+      ]
+    },
+    {
+      "id": "5-entrada",
+      "name": "entrada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9745082611404907,
+          "pitch": -0.09457795133828562,
+          "rotation": 0,
+          "target": "6-visual-patio"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.042147801500640014,
+          "pitch": 0.12991007233943286,
+          "title": "Entrada do Gramado Motorpark",
+          "text": "Rua dos pequineses 102"
+        },
+        {
+          "yaw": -2.381295973786191,
+          "pitch": 0.017267976231051918,
+          "title": "Hortensias",
+          "text": "No periodo de setembro a dezembro as hortensias são uma linda atração da nossa cidade. E aqui no Motorpark elas não deixam a desejar."
+        }
+      ]
+    },
+    {
+      "id": "6-visual-patio",
+      "name": "visual patio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4291923533102109,
+          "pitch": 0.02899280970742879,
+          "rotation": 0,
+          "target": "9-vagas"
+        },
+        {
+          "yaw": 0.6607179028181367,
+          "pitch": 0.1572981059368388,
+          "rotation": 0,
+          "target": "3-estrada"
+        },
+        {
+          "yaw": -0.7781807369480038,
+          "pitch": 0.06291959276356707,
+          "rotation": 0,
+          "target": "5-entrada"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.4825427057049723,
+          "pitch": -0.11630298918134763,
+          "title": "Viaggio di Luxo, onibus com banheira",
+          "text": "Nosso maior onibus para hospedagem, um verdaeiro luxo!"
+        }
+      ]
+    },
+    {
+      "id": "7-texaco-frente",
+      "name": "texaco frente",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.101850600096812,
+          "pitch": 0.1891332993913366,
+          "rotation": 0,
+          "target": "0-oficina-do-churrasco"
+        },
+        {
+          "yaw": 0.04966425664247609,
+          "pitch": 0.12543733942038138,
+          "rotation": 0,
+          "target": "1-texaco"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.22974231990537497,
+          "pitch": 0.15518728371290003,
+          "title": "Texaco Gramado",
+          "text": "Area externa é uma area comum para todos os clientes. podem tirar fotos, aproveitar o espaço com a cozinha comum e a churrasqueira. E a parte interna é alugada por diária, com dois quartos com cama de casal."
+        },
+        {
+          "yaw": 1.1319662925200316,
+          "pitch": 0.1314448151149783,
+          "title": "Banheiro comum",
+          "text": "Outro banheiro de uso comum."
+        },
+        {
+          "yaw": 1.52936373015479,
+          "pitch": 0.07692752927776247,
+          "title": "Lavanderia",
+          "text": "A lavanderia é um serviço que pode ser utilizado pelos hospedes com custos adicinais."
+        }
+      ]
+    },
+    {
+      "id": "8-pedra-alta",
+      "name": "pedra alta",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-vagas",
+      "name": "vagas",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3815915887387078,
+          "pitch": 0.08752605266769287,
+          "rotation": 0,
+          "target": "0-oficina-do-churrasco"
+        },
+        {
+          "yaw": -2.637254181066675,
+          "pitch": 0.025470342021213455,
+          "rotation": 0,
+          "target": "6-visual-patio"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.848668196373975,
+          "pitch": 0.020401710102451176,
+          "title": "caixa de Luz",
+          "text": "tomadas 220v. Além dessa temos outras 2 caixas de luz nas vagas."
+        },
+        {
+          "yaw": 2.462110628751681,
+          "pitch": 0.002679863255201198,
+          "title": "Agua e descarte de agua negra",
+          "text": "são varias torneiras para as vagas"
+        },
+        {
+          "yaw": 0.6627182304890376,
+          "pitch": 0.00945750022392211,
+          "title": "Vaga para trailer ou Motorhome",
+          "text": "São 10 vagas exclusivas para trailer ou motorhome, aqui não temos avgas para barracas. Local familiar e tranquilo."
+        },
+        {
+          "yaw": -1.1185824550154972,
+          "pitch": 0.03224292177917221,
+          "title": "Trailer Brilhante",
+          "text": "Nossa hospedagem mais alugada. O trailer brilhante une uma experiencia de conforto a preço justo."
+        },
+        {
+          "yaw": 2.8644193775097566,
+          "pitch": -0.029631875105893712,
+          "title": "Banheiro de vidro",
+          "text": "Um dois banheiros comuns do Gramado motorpark, no meio do mato e todo feito de vidro, uma experiencia diferente!"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
